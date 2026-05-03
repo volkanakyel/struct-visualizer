ceacf3d85e0443990d8a4bf6e30fd7b0d853b31c
Volkan Akyel <volkan@uikielle.com>
2026-05-03 13:40:30 +0200

    Update Tailwind CSS configuration to include new content paths for components, layouts, pages, and composables, enhancing the project's styling capabilities.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./public/**/*.html"
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.{vue,js,ts}',
+    './app.vue',
+    './composables/**/*.{js,ts}',
   ],
   theme: {
     extend: {
